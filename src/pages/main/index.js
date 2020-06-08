299b67bee026912f9d62ec21be6518b026b66aa8
Dmitriy <ggbox@mail.ru>
2020-06-08 13:15:27 +0400

    add

---
+++ src/pages/main/index.js
@@ -0,0 +1,41 @@
+/*
+let fuckYou = "Fuck You";
+let man = "and suck my dick";
+let space = " ";
+let fucker = {
+  name: "",
+  age: "",
+};
+alert(fuckYou + space + man);
+fucker.name = prompt("What is Your name, bitch?");
+
+fucker.age = prompt("How old are You, bitch&");
+console.log(fucker);
+*/
+
+
+
+/*let number = 0;
+
+number = prompt("write the number, bitch");
+for(let i=0; i<=number;i++){
+  if(i%4){
+    console.log(i)
+  }
+}
+*/
+
+/*let number = 0;
+
+let growSoft = setInterval(function(){
+  number+= 3;
+  console.log(number)},3000)
+
+
+
+setTimeout(function(){clearInterval(growSoft)}, 9000)
+*/
+
+
+
+
